964099bb30417b50ea8eaf4d965e7348f6c498c7
Matheus Adler <matheusadler@gmail.com>
2022-06-21 23:03:24 -0300

    Implementação da tela de index e suas funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// Carregando os módulos do framework Electron
+// carregando os módulos do framework Electron
 const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 
-// Este método irá instanciar a janela principal da aplicação
+// este método irá instanciar a janela principal da aplicação
 function createWindow(){    
     const mainWindow = new BrowserWindow({
         width: 1280,
@@ -14,21 +14,59 @@ function createWindow(){
         }
     });
         
-    // Carrega o arquivo index.html
+    // carrega o arquivo index.html
     mainWindow.loadFile('./html/index.html');
 }
 
 app.whenReady().then(() => {
-    // Após o Electron ser devidamente carregado, ele chamará o método abaixo
+    // após o Electron ser devidamente carregado, ele chamará o método abaixo
     createWindow();
     app.on('activate', () => {
-        // No macOS, é comum recriar uma janela no aplicativo quando o ícone do dock é clicado e não há outras janelas abertas.
+        // no macOS, é comum recriar uma janela no aplicativo quando o ícone do dock é clicado e não há outras janelas abertas.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
       })
 });
 
-// Encerra a aplicação se todas as suas janelas forem fechadas, caso o kernel atual não seja "darwin" (MacOs)
-// No MacOS, é comum que os aplicativos e sua barra de menus permaneçam ativos até que o usuário saia explicitamente através do comando Cmd + Q.
+// encerra a aplicação se todas as suas janelas forem fechadas, caso o kernel atual não seja "darwin" (MacOs)
+// no MacOS, é comum que os aplicativos e sua barra de menus permaneçam ativos até que o usuário saia explicitamente através do comando Cmd + Q.
 app.on("window-all-closed", ()=>{
     if(process.platform !== "darwin") app.quit();
+})
+
+// evento para salvar os arumentos selecionados
+ipcMain.on("save-options", (args) => { 
+    options = args[0]
+});
+
+// evento para acessar os arumentos salvos
+ipcMain.on("get-options", (event) => {
+    event.returnValue = [options]
+});
+
+// evento para salvar a quantidade de vídeos buscados na pesquisa
+ipcMain.on("save-video-length", (args) => {
+    text = args[0]
+});
+
+// evento para acessar a quantidade de vídeos buscados na pesquisa
+ipcMain.on("get-video-length", (event) => {
+    event.returnValue = [text]
+});
+
+// evento para salvar a quantidade de comentários coletados
+ipcMain.on("save-comments-length", (args) => {
+    text = args[0]
+});
+
+// evento para acessar a quantidade de comentários coletados
+ipcMain.on("get-comments-length", (event) => {
+    event.returnValue = [text]
+});
+
+// quando o evento 'select-directory' for chamado a janela de diretórios é aberta e, em seguida, retorna o caminho especificado
+ipcMain.on('select-directory', (event, arg) => {
+    selected_dir = dialog.showOpenDialogSync({
+        properties: ['openDirectory']
+    });
+    event.returnValue = selected_dir
 })
